da66ea647fbcb5d163c25e622be5ec1904acd27a
Nathan Lanza <nathanlanza@gmail.com>
2025-12-03 13:52:30 -0500

    lua

--- runtime/queries/lua/folds.scm
+++ runtime/queries/lua/folds.scm
@@ -10,3 +10,19 @@
   (arguments)
   (table_constructor)
 ] @fold
+
+; Fold the initial if block (between "if" and first "elseif"/"else"/"end")
+(if_statement
+  consequence: (block) @fold)
+
+; Fold individual elseif blocks
+(elseif_statement
+  consequence: (block) @fold)
+
+; Fold individual else blocks
+(else_statement
+  (block) @fold)
+
+; Also fold the entire elseif/else statements for structure visibility
+(elseif_statement) @fold
+(else_statement) @fold
