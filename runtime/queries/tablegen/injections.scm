72d39229cc562c8a620af571c9f6a7f788bb7e61
Nathan Lanza <nathanlanza@gmail.com>
2025-12-03 13:41:58 -0500

    tablegen

--- runtime/queries/tablegen/injections.scm
+++ runtime/queries/tablegen/injections.scm
@@ -1,9 +1,19 @@
-((code) @injection.content
-  (#set! injection.language "cpp")
-  (#offset! @injection.content 0 2 0 -2))
+; Injections queries for TableGen
+; These patterns enable syntax highlighting of embedded languages
 
-((tablegen_file
-  (comment) @injection.content)
-  (#lua-match? @injection.content "^.*RUN")
-  (#set! injection.language "bash")
-  (#offset! @injection.content 0 8 0 0))
+; Code strings often contain C++ code in LLVM TableGen files
+; The code_string node contains code between [{ and }]
+; We inject C++ highlighting into these regions
+((code_string) @injection.content
+  (#set! injection.language "cpp")
+  (#set! injection.include-children))
+
+; Comments might contain documentation markup
+; Some projects use doxygen-style comments
+((comment) @injection.content
+  (#match? @injection.content "^//[/<]")
+  (#set! injection.language "doxygen"))
+
+((multiline_comment) @injection.content
+  (#match? @injection.content "^/\\*[\\*!]")
+  (#set! injection.language "doxygen"))
