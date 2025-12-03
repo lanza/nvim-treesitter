47432777899364127b16443c0b3b3d4c1461e4da
Nathan Lanza <nathanlanza@gmail.com>
2025-12-03 15:56:34 -0500

    vim

--- runtime/queries/vim/folds.scm
+++ runtime/queries/vim/folds.scm
@@ -1,4 +1,14 @@
+; Foldable constructs in VimScript
 [
-  (if_statement)
   (function_definition)
+  (if_statement)
+  (for_loop)
+  (while_loop)
+  (try_statement)
+  (augroup_statement)
+  (dictionnary)
+  (list)
+  (heredoc)
+  (lambda_expression)
+  (script)
 ] @fold
